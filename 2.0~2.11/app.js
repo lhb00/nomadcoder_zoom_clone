04e13432c26f2eabcca007746395f5553bf6f014
이희범 <71597554+lhb00@users.noreply.github.com>
2022-08-26 18:29:44 +0900

    2.7강 코드 챌린지 내용 반영.

--- 2.0~2.11/app.js
+++ 2.0~2.11/app.js
@@ -1,6 +1,7 @@
 const socket = io(); // 자동으로 backend socket.io와 연결해주는 함수
 const welcome = document.getElementById("welcome");
-const form = welcome.querySelector("form");
+const roomNameForm = welcome.querySelector("#roomName");
+const nicknameForm = welcome.querySelector("#name");
 const room = document.getElementById("room");
 
 room.hidden = true;
@@ -27,7 +28,7 @@ function handleMessageSubmit(event) {
 
 function handleNicknameSubmit(event) {
     event.preventDefault();
-    const input = room.querySelector("#name input"); // nameform 안에 있는 input만 가져옴
+    const input = nicknameForm.querySelector("input"); // nameform 안에 있는 input만 가져옴
     socket.emit("nickname", input.value);
 }
 
@@ -37,14 +38,12 @@ function showRoom() {
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
     const msgForm = room.querySelector("#msg");
-    const nameForm = room.querySelector("#name");
     msgForm.addEventListener("submit", handleMessageSubmit);
-    nameForm.addEventListener("submit", handleNicknameSubmit);
 } // 그런데, SocketIO는 여기 frontend에서 작성한 함수를,
 
 function handleRoomSubmit(event) {
     event.preventDefault();
-    const input = form.querySelector("input");
+    const input = roomNameForm.querySelector("input");
     socket.emit("enter_room", input.value, showRoom); // emit은 socket.send와 다르게 event를 emit 해줄 수 있음. 이름은 중요치 않음. 물론 socket.on과 socket.emit에서 당연히 같은 이름을 사용해야 한다.
     // 또한, argument를 보낼 수 있음. 그리고 argument는 object라고 볼 수 있음!
     // WebSocket을 사용할 당시에 Object를 보내려면 Object를 string으로 바꾸고 다시 Object로 바꿔서 보내야 했지만, 이젠 그럴 필요가 없음!
@@ -55,7 +54,9 @@ function handleRoomSubmit(event) {
     roomName = input.value;
     input.value = ""
 }
-form.addEventListener("submit", handleRoomSubmit);
+
+roomNameForm.addEventListener("submit", handleRoomSubmit);
+nicknameForm.addEventListener("submit", handleNicknameSubmit);
 
 socket.on("welcome", (user, newCount) => {
     const h3 = room.querySelector("h3");
