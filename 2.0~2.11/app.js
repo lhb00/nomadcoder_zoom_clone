502cbf4ce957e0fe30d7a275a6a4acde8200e1e9
이희범 <71597554+lhb00@users.noreply.github.com>
2022-08-26 15:12:22 +0900

    2.6~2.11 강의 내용 정리 추가 정리 완료.

--- 2.0~2.11/app.js
+++ 2.0~2.11/app.js
@@ -14,11 +14,32 @@ function addMessage(message) {
     ul.appendChild(li);
 }
 
+function handleMessageSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#msg input"); // msgform 안에 있는 input만 가져옴
+    const value = input.value; // 그냥 input.value 값을 쓰고 비워주면 메시지가 나타나지 않는 오류가 발생하기 때문에 따로 value라는 변수 지정
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value = "";
+} // Backend로 메시지 보내기, 마지막 argument는 Backend에서 실행시킬 수 있는 함수
+// roomName 변수가 없으면 어떤 방에 보내는지 알 수 없으므로 필히 추가해주어야함!
+
+function handleNicknameSubmit(event) {
+    event.preventDefault();
+    const input = room.querySelector("#name input"); // nameform 안에 있는 input만 가져옴
+    socket.emit("nickname", input.value);
+}
+
 function showRoom() {
     welcome.hidden = true;
     room.hidden = false;
     const h3 = room.querySelector("h3");
     h3.innerText = `Room ${roomName}`;
+    const msgForm = room.querySelector("#msg");
+    const nameForm = room.querySelector("#name");
+    msgForm.addEventListener("submit", handleMessageSubmit);
+    nameForm.addEventListener("submit", handleNicknameSubmit);
 } // 그런데, SocketIO는 여기 frontend에서 작성한 함수를,
 
 function handleRoomSubmit(event) {
@@ -36,6 +57,30 @@ function handleRoomSubmit(event) {
 }
 form.addEventListener("submit", handleRoomSubmit);
 
-socket.on("welcome", () => {
-    addMessage("someone joined!");
+socket.on("welcome", (user, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`; // title을 변경(숫자가 계속 바뀌어야하니까) 해주기 위해 여기에 추가함.
+    // 마지막엔 title을 변경해주는 함수를 구현해볼거임.
+    addMessage(`${user} arrived!`);
 }); // 더 이상 addEventListener는 필요 없다. 바닐라 JS 코드 탈출!
+
+socket.on("bye", (left, newCount) => {
+    const h3 = room.querySelector("h3");
+    h3.innerText = `Room ${roomName} (${newCount})`;
+    addMessage(`${left} left ㅠㅠ`);
+});
+
+socket.on("new_message", addMessage); // addMessage를 그냥 호출하는 것은 (msg)=> {addMessage(msg)}라고 적는 것과 같다. 어차피 addMessage만 써도 argument와 함께 function을 호출하기 때문
+
+socket.on("room_change", (rooms) => {
+    const roomList = welcome.querySelector("ul");
+    roomList.innerHTML = "";
+    if(rooms.length === 0){
+        return;
+    } // room이 하나도 없을 때 room이 여전히 잔존하는 문제를 해결하기 위하여 추가한 코드
+    rooms.forEach(room => {
+        const li = document.createElement("li");
+        li.innerText = room;
+        roomList.append(li);
+    });
+});
--- 2.0~2.11/server.js
+++ 2.0~2.11/server.js
@@ -1,5 +1,6 @@
 import http from "http";
-import SocketIO from "socket.io";
+import {Server} from "socket.io";
+import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 const app = express();
@@ -13,17 +14,56 @@ app.get("*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`); 
 const httpServer = http.createServer(app); // http 서버와
-const wsServer = SocketIO(httpServer); // WebSocket 서버를 둘 다 돌릴 수 있음! 
+const wsServer = new Server(httpServer, {
+    cors: {
+      origin: ["https://admin.socket.io"], // 이 URL에서 localhost:3000에 액세스 할 거임.
+      credentials: true
+    },
+}); // WebSocket 서버를 둘 다 돌릴 수 있음! 
+
+instrument(wsServer, {
+    auth: false
+});
+
+function publicRooms(){
+    const {sockets: {adapter: {sids, rooms}}} = wsServer; // Adapter로 부터 sids와 rooms를 가져옴.
+    const publicRooms = [];
+    rooms.forEach((_, key) => {
+        if(sids.get(key) === undefined){
+            publicRooms.push(key);
+        } // Rooms의 key들 중 sids에 없는 key, 즉 Socket ID가 아닌 key를 Array에 추가해주고,
+    });
+    return publicRooms; // Array를 반환
+}
+
+function countRoom(roomName) {
+    return wsServer.sockets.adapter.rooms.get(roomName)?.size // roomName을 못찾는 경우도 있으므로 ? 추가
+}
 
 wsServer.on("connection", (socket) =>{
+    socket["nickname"] = "Anon";
     socket.onAny((event) => {
+        console.log(wsServer.sockets.adapter);
         console.log(`Socket Event: ${event}`);
     });
     socket.on("enter_room", (roomName, done) => {
         socket.join(roomName); // 방에 들어가기 위해 사용. SocketIO가 기본으로 제공.
         done(); // Backend에서도 실행해주어야 원하는 결과를 얻을 수 있다.
-        socket.to(roomName).emit("welcome"); // 방에 입장하면 본인을 제외한 방에 있는 모든 사람에게 'welcome'이라는 메시지가 전달됨.
+        socket.to(roomName).emit("welcome", socket.nickname, countRoom(roomName)); // 방에 입장하면 본인을 제외한 방에 있는 모든 사람에게 'welcome'이라는 메시지가 전달됨.
+        // 하나의 socket에만 메시지가 전달됨.
+        wsServer.sockets.emit("room_change", publicRooms()); // 모든 socket에 메시지가 전달됨.
      }); // socket.on 뒤에 우리가 원하는 이벤트를 넣어주기만 하면 된다.
+     socket.on("disconnecting", () => { // disconneting event로 방에서 떠났을 때
+        socket.rooms.forEach(room => socket.to(room).emit("bye", socket.nickname, countRoom(room) - 1)); // socket.rooms로 중복되는 요소가 없는, 각 방의 이름이 담긴 array를 받을 수 있으므로 foreach 사용 가능.
+     }); // disconnecting 함수는 연결이 종료되기 직전에 발생하므로 여기에 방을 떠났을 때 방 이름을 보내는 동작을 추가 시 방이 여전히 남아 있는 것으로 나옴. 그래서 -1을 해주는 것.
+     socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
+     }); // 따라서 종료 후에 실행되는 disconnect 함수를 대신 사용함
+     socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
+        done();
+     }); // Backend에서 message 받고, done 함수는 마찬가지로 frontend에서 실행
+     socket.on("nickname", (nickname) => (socket["nickname"] = nickname)); // nickname event가 발생하면 nickname을 가져와서 socket에 저장
 });// 각각 다른 브라우저에서 접속 시 socket이 다르므로 여러번 실행될 것이다.
 
 // argument로 서버를 전달해 줄 수 있으나, 필수는 아님!
